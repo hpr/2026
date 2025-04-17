9d8348ee00991b4212e3e71196bf5babf00c79e8
Harry <habs@sdf.org>
2025-04-16 22:12:17 -0400

    align buttons

--- src/ColorSchemeToggle.tsx
+++ src/ColorSchemeToggle.tsx
@@ -1,15 +1,16 @@
-import { useMantineColorScheme } from '@mantine/core';
+import { Button, useMantineColorScheme } from '@mantine/core';
 import { Sun, Moon } from 'tabler-icons-react';
 
 export function ColorSchemeToggle() {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
 
   return (
-    <button 
+    <Button 
+      size="xs"
       onClick={() => toggleColorScheme()}
-      style={{ marginLeft: '10px', marginRight: '-10px', cursor: 'pointer', padding: '5px' }}
+      style={{ marginLeft: '10px', marginRight: '-10px', padding: '5px' }}
     >
       {colorScheme === 'dark' ? <Sun size={10} /> : <Moon size={10} />}
-    </button>
+    </Button>
   );
 }
